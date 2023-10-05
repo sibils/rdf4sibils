e480616a6d12beebed1ec07a513fce03a72b6e1f
SuperPAM <superpam@sibils-sparql.lan.text-analytics.ch>
2023-10-05 16:40:31 +0200

    adding scripts related to loading chunks into triple store

---
+++ empty_db.sql
@@ -0,0 +1,21 @@
+
+-- parameters for SPONGE functionality
+
+grant select on "DB.DBA.SPARQL_SINV_2" to "SPARQL";
+grant execute on "DB.DBA.SPARQL_SINV_IMP" to "SPARQL";
+GRANT SPARQL_SPONGE TO "SPARQL";
+GRANT EXECUTE ON DB.DBA.L_O_LOOK TO "SPARQL";
+
+-- empty sibils graph
+
+log_enable(3,1);
+SPARQL CLEAR GRAPH <http://sibils.org/rdf>; -- took ~4 minutes for 25 mega triples
+checkpoint;
+
+-- empty load list
+
+delete from DB.DBA.load_list;
+select * from DB.DBA.load_list;
+
+-- done
+
---
+++ load_chunk.sh
@@ -0,0 +1,34 @@
+if [ "$1" == "" ]; then 
+  echo "ERROR, usage is $0 <chunk_name>"
+  exit 1
+fi
+
+echo "DBA_PW: ${DBA_PW}"
+
+if [ "${DBA_PW}" == "" ]; then
+  echo "ERROR, please define and export environment variable DBA_PW"
+  exit 2
+fi
+
+chunk=$1
+chunk_dir=/share/rdf/ttl/${chunk}
+
+echo ".......... doing the job ${chunk}"
+exit
+
+echo "$(date) - Decompressing files in $chunk_dir"
+
+lz4 -m -d ${chunk_dir}/*.ttl.lz4
+
+echo "$(date) - Adding files to virtuoso load list and loading"
+
+isql-vt 1111 dba $DBA_PW "EXEC=ld_dir ('/share/rdf/ttl/${chunk}', '*.ttl', 'http://sibils.org/rdf') ;"
+isql-vt 1111 dba $DBA_PW "EXEC=select * from DB.DBA.load_list;"
+isql-vt 1111 dba $DBA_PW "EXEC=rdf_loader_run();"
+
+echo "$(date) - Deleting decompressed files"
+
+rm ${chunk_dir}/*.ttl
+
+echo "$(date) - Done"
+
