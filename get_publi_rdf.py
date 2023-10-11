6b7ed32802169ba6492d53103fb8153303d16035
pmichel <pamichel@infomaniak.ch>
2023-10-12 01:08:31 +0200

    adapted URI maker for sentences

--- get_publi_rdf.py
+++ get_publi_rdf.py
@@ -365,7 +365,8 @@ def get_part_title_URIRef(part_uri):
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 def get_sentence_part_URIRef(publi_uri, sentence_or_annot):
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    part_id = str(sentence_or_annot.get("sentence_number"))
+    part_id = str(sentence_or_annot.get("id_sentence"))
+    if part_id is None: part_id = str(sentence_or_annot.get("sentence_number"))
     return publi_uri + "_sen_" + part_id
 
 
