04b7251c492ff59f344e88bca26dca1cff2c26c5
pmichel <pamichel@infomaniak.ch>
2023-10-19 16:51:36 +0200

    fixed syntax error

--- load_chunk.sh
+++ load_chunk.sh
@@ -19,22 +19,30 @@ echo "$(date) - Decompressing files in $chunk_dir"
 
 msg=""
 lz4 -m -d ${chunk_dir}/*.ttl.lz4
-if [ "$?" != "0" ]; then msg="ERROR while decompressing files in $chunk_dir"; fi
-exit 4
-
+if [ "$?" != "0" ]; then 
+  msg="ERROR while decompressing files in $chunk_dir"; 
+  exit 4
+fi
+  
 echo "$(date) - Adding files to virtuoso load list and loading"
 
 isql-vt 1111 dba $DBA_PW "EXEC=ld_dir ('/share/rdf/ttl/${chunk}', '*.ttl', 'http://sibils.org/rdf') ;"
-if [ "$?" != "0" ]; then msg="ERROR while adding files to load list with isql for chunk $chunk"; fi
-exit 5
+if [ "$?" != "0" ]; then 
+  msg="ERROR while adding files to load list with isql for chunk $chunk"; fi
+  exit 5
+fi
 
 isql-vt 1111 dba $DBA_PW "EXEC=select * from DB.DBA.load_list where ll_file like '*pmc*' ;"
-if [ "$?" != "0" ]; then msg="ERROR while reading list of files to load with isql for chunk $chunk"; fi
-exit 6
+if [ "$?" != "0" ]; then 
+  msg="ERROR while reading list of files to load with isql for chunk $chunk"; fi
+  exit 6
+fi
 
 isql-vt 1111 dba $DBA_PW "EXEC=rdf_loader_run();"
-if [ "$?" != "0" ]; then msg="ERROR while running load with isql for chunk $chunk"; fi
-exit 7
+if [ "$?" != "0" ]; then 
+  msg="ERROR while running load with isql for chunk $chunk"; 
+  exit 7
+fi
 
 echo "$(date) - Deleting decompressed files"
 
