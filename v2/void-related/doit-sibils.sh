50b5c654437a27af635850c7552bc705fb582ee6
pmichel <pamichel@infomaniak.ch>
2025-08-27 16:12:01 +0200

    moved to void-generator version 0.9 and adapted script

--- v2/void-related/doit-sibils.sh
+++ v2/void-related/doit-sibils.sh
@@ -5,8 +5,10 @@ set -e
 script_dir="$(dirname $0)"
 cd $script_dir
 
-java -jar target/void-generator-0.1-SNAPSHOT.jar --user=dba --password=dba  \
-    -r jdbc:virtuoso://localhost:1112/charset=UTF-8  \
+java -jar void-generator.jar \
+    --user=dba \
+    --password=dba \
+    --virtuoso-jdbc=jdbc:virtuoso://localhost:1112/charset=UTF-8 \
+    -r "http://localhost/sparql/sparql"  \
     -s void-sibils.ttl \
-    -i "http://localhost:8891/sparql/.well-known/void"
-#    -i "https://sparql.cellosaurus.org/.well-known/void"
+    -i "http://localhost/sparql/.well-known/void"
