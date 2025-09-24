7bf395da79dd52342c4c65610763e88481d7f141
pmichel <pamichel@infomaniak.ch>
2025-09-24 13:53:39 +0200

    adapted script to local access of terminology files

--- v2/rdfize/get_terminologies.sh
+++ v2/rdfize/get_terminologies.sh
@@ -7,16 +7,30 @@ cd $this_dir
 
 echo "Get a local copy of terminologies..."
 
-
 remote_server="superpam@rdfizer.lan.text-analytics.ch"
-remote_files_location="/data/terminologies/v.t8/current-release"
+files_location="/data/terminologies/v.t8/current-release"
 target_dir="../out/terminologies/"
+mode=$1
 
+echo "source dir: $files_location"
 echo "target dir: $target_dir"
+echo "copy mode : $mode"
 
 mkdir -p $target_dir
-ssh $remote_server "tar czvf terminologies.tar.gz -C /data/terminologies/v.t8/current-release ."
-scp $remote_server:terminologies.tar.gz $target_dir
-cd $target_dir
-tar xvzf terminologies.tar.gz
+
+if [ "$mode" = "remote"  ]; then
+  ssh $remote_server "tar czvf terminologies.tar.gz -C $files_location ."
+  scp $remote_server:terminologies.tar.gz $target_dir
+  cd $target_dir
+  tar xvzf terminologies.tar.gz
+elif [ "$mode" = "local" ]; then
+  cp $files_location/* $target_dir
+else
+  echo "Error: usage is $0 local|remote"
+  exit 1
+fi
+
+
+
+
 
